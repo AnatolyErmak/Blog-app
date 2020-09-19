494a2e20eb0d1c6aa39b1fec3e1e65a191df805b
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-20 01:56:05 +0300

    Свойства и состояния компонентов. События в React

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,14 +9,21 @@ import PostAddForm from '../post-add-form/post-add-form'
 import './app.css'
 
 const App = () => {
+
+    const data = [
+        {label:'Going to learn React', important: true , id: 'dfdf'},
+        {label:'Thst is so good', important: false , id: 'vhfwd'},
+        {label:'I need a break...', important: false , id: 'sdfsdfsdf' }
+    ]
+
     return (
         <div className = "app">
             <AppHeader/>
-            <div   div className = "search-panel d-flex">
+            <div className = "search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts = {data}/>
             <PostAddForm/>
         </div>
     )
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,25 @@ import React from 'react';
 import PostListItem from '../post-list-item/post-list-item';
 import './post-list.css'
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+
+        const {id, ...itemProps} = item; // деструктуризация разбиваем наш пропс на 2 элемента
+
+        return (
+            <li key={id} className = "list-group-item">
+                <PostListItem {...itemProps} /*  это тоже самое 
+                label = {item.label} 
+                important = {item.important} */
+                />
+            </li>
+        )
+    })
+
     return (
         <ul className = "app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
