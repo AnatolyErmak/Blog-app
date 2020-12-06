f2ef5e086512507c464f15c01c6479d3914012e9
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-12-06 21:07:31 +0300

    знакомство styled-componets, comeback

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,15 @@ import PostList from '../post-list/post-list'
 import PostAddForm from '../post-add-form/post-add-form'
 
 import './app.css'
+import styled from 'styled-components';
 
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+const StyledAppBlock = styled(AppBlock)`
+    background-color: grey;
+`
 const App = () => {
 
     const data = [
@@ -17,7 +25,7 @@ const App = () => {
     ]
 
     return (
-        <div className = "app">
+        <AppBlock>
             <AppHeader/>
             <div className = "search-panel d-flex">
                 <SearchPanel/>
@@ -25,7 +33,7 @@ const App = () => {
             </div>
             <PostList posts = {data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
